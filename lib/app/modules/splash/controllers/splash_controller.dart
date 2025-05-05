4817e135a55f21d68e3efc4144075ed949594c38
binimuse <bini.musema@gmail.com>
2025-05-05 07:51:45 +0300

    first commit

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
-
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:muya/app/routes/app_pages.dart';
 import 'package:muya/gen/assets.gen.dart';
+import 'package:muya/app/services/telegram_service.dart';
 
 class SplashController extends GetxController {
   final String splasehimage = Assets.logos.logo.path;
+  final TelegramService telegramService = Get.find<TelegramService>();
 
   @override
   void onInit() {
@@ -13,18 +15,17 @@ class SplashController extends GetxController {
   }
 
   Future<void> _initializeApp() async {
-    await Future.delayed(const Duration(seconds: 3));
-    Get.offNamed(Routes.HOME);
-    // First check notification permission
-    // final notificationPermissionStatus = await requestNotificationPermission();
+    // Shorter delay for web
+    await Future.delayed(Duration(seconds: kIsWeb ? 1 : 3));
 
-    // if (notificationPermissionStatus == PermissionStatus.granted) {
-    //   await _checkAuthAndNavigate();
-    // } else {
-    //   AppToasts.showError("Please Enable Permissions".tr);
-    //   await openAppSettings();
-    //   // After opening settings, check again
-    //   await _checkAuthAndNavigate();
-    // }
+    // For web, check if Telegram WebApp is initialized
+    if (kIsWeb) {
+      if (telegramService.userName.value.isEmpty) {
+        // If Telegram WebApp is not available, show error and proceed
+        print('Telegram WebApp not initialized properly');
+      }
+    }
+
+    Get.offNamed(Routes.HOME);
   }
 }
--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:muya/app/theme/app_colors.dart';
-
 import 'package:responsive_sizer/responsive_sizer.dart';
-
+import 'package:flutter/foundation.dart' show kIsWeb;
 import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
@@ -13,25 +11,24 @@ class SplashView extends GetView<SplashController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Container(color: AppColors.primary),
-          Align(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  controller.splasehimage,
-                  fit: BoxFit.contain,
-                  width: 40.w,
-                ),
-                SizedBox(height: 1.h),
-              ],
-            ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: AppColors.primary,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                controller.splasehimage,
+                fit: BoxFit.contain,
+                width: kIsWeb ? 30.w : 40.w,
+                height: kIsWeb ? 30.w : 40.w,
+              ),
+              if (!kIsWeb) SizedBox(height: 1.h),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
